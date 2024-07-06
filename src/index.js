dbb96e4620f9e547878fde852acd9be695891a7f
Sudhansu-kumar <91793830+Sudhansu-kumar@users.noreply.github.com>
2024-07-06 13:17:01 +0530

    added util files

--- src/index.js
+++ src/index.js
@@ -11,5 +11,16 @@ dotenv.config({
 })
 
  connectionDB()
-// console.log(process.env.MONGODB_URL);
-// console.log(DB_NAME); 
+.then(() => {
+    app.on("error" , (error) => {
+        console.log("Error: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 3000 , () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection Failed! : ", error);
+})
